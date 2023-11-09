d857732f85c90698130503b624caa93f6c9f21e7
Ronald Ponferrada <ronaldponferrada@Ronalds-MacBook-Air.local>
2023-11-10 01:16:04 +0900

    added alumni fellows and more to come

--- _site/assets/js/mustache-fellows-render.js
+++ _site/assets/js/mustache-fellows-render.js
@@ -269,9 +269,48 @@ let nicholView = {
     bio2: "As a Senior Innovation Fellow, Dr. Salvo's mission is to provide basic foot care to patients at increased risk for amputation, improve access and timeliness of care, and prevent amputation in Veterans, establishing a model of care that can be used nationally across VA. This is to be accomplished by developing a permanent program using telehealth and tele-supervision, which allows podiatrists to serve more patients than in traditional face-to-face clinical settings."
 };
 
+let bethView = {
+    id: "beth-ripley",
+    fellow_type: "sif",
+    fellow_status: "alumni",
+    img: "https://www.innovation.va.gov/ecosystem/assets/Images/IE/fellows/BethRipley.jpg",
+    profileImg: "https://www.innovation.va.gov/ecosystem/assets/Images/IE/fellows/BethRipley.jpg",
+    alt: "Beth Ripley, M.D., Ph.D.",
+    bio_name: "Beth Ripley, M.D., Ph.D.",
+    title: "2019 Senior Innovation Fellow",
+    job: "",
+    job2: "",
+    job_description: "",
+    office_title: "Acting Chief Officer",
+    office_location: "VHA Office of Healthcare Innovation and Learning",
+    project: "3D Printing Network",
+    bio: "Dr. Beth Ripley oversees strategic operations to advance innovation across VHA. She is a leader in the expansion of 3D Printing (3DP) technology capabilities and serves as the Enterprise Lead for the VHA 3D Printing Network Initiative, Director for VA Ventures and serves on the VHA Advanced Manufacturing Advisory Committee. Dr. Ripley collaborates with talented and diverse staff throughout VA, experts across federal agencies, and industry partners to improve the delivery of personalized healthcare solutions for Veterans through 3DP. Dr. Ripley is a staff radiologist at the Seattle Division of the VA Puget Sound Health Care System and an Assistant Professor of Radiology at the University of Washington School of Medicine. She received her Bachelor's Degree in Art History from Stanford University as well as her M.D. and Ph.D. in Neurosciences from University of California, San Diego School of Medicine. She completed radiology residency training and a cardiovascular imaging fellowship at Brigham and Women's Hospital, Harvard Medical School, and a body imaging fellowship at the University of Washington."
+};
+
+let michaelView = {
+    id: "michael-amendola",
+    fellow_type: "sif",
+    fellow_status: "alumni",
+    img: "https://www.innovation.va.gov/ecosystem/assets/Images/IE/fellows/MichaelAmendola.jpg",
+    profileImg: "https://www.innovation.va.gov/ecosystem/assets/Images/IE/fellows/MichaelAmendola.jpg",
+    alt: "Michael Amendola, M.D., M.E.H.P.",
+    bio_name: "Michael Amendola, M.D., M.E.H.P.",
+    title: "2021 Senior Innovation Fellow",
+    job: "",
+    job2: "",
+    job_description: "",
+    office_title: "Chief of Vascular Surgery",
+    office_location: "Central Virginia VA Healthcare System",
+    project: "3D Printing Pre-Surgical Planning Curriculum",
+    bio: "Dr. Michael Amendola is the Chief of Vascular Surgery, Director of the Non-Invasive Vascular Lab and Program Director for the Chief Resident in Quality and Patient Safety at the Central Virginia VA Health Care System. He is a Professor of Surgery at Virginia Commonwealth University School of Medicine and Adjunct Faculty at Johns Hopkins School of Education. As a Senior Innovation Fellow, Dr. Amendola created a local 3D Printing (3DP) for pre-surgical planning curriculum for surgeons and contributed to launching a national VHA training course to introduce healthcare teams to 3DP technology. He is the program director of the Office of Advance Manufacturing's Central Virginia VA Health Care System based 3DP Surgical Fellowship. His interests focus on mentoring novice surgical learners, hemodialysis access, 3D printing and implications of surgical simulation in the current health care environment.",
+    bio2: "Dr. Michael Amendola received his medical degree at the Virginia Commonwealth University School of Medicine (VCU-SOM) in Richmond, Virginia. He completed his general surgery internship and residency at Virginia Commonwealth University Health System (VCUHS) in 2007. He also completed a Vascular Surgery fellowship at VCUHS in 2009. He is a fellow of the American College of Surgeons and the Society of Vascular Surgery. In 2021 he was inducted into the American College of Surgeons Academy of Master Surgeon Educators. Dr. Amendola is board certified by the American Board of Surgery in both general and vascular surgery. He maintains privileges at Central Virginia VA Health Care System in Richmond, Virginia and is Professor of Surgery at VCU-SOM. He completed his Master's Degree in Medical Education at John Hopkins School of Education in Baltimore, Maryland in 2019."
+};
+
+
+
 
 // Mustache render logic for individual page views | ?user="firstname-lastname"
-var fellowsArray = new Array(candisView, jacquelynView, melissaView, aksharView, taylorView, stacyView, priyaView, kevinView, elizabethView, jayView, minhView, maryView, loriView, ronaView, nicholView);
+var fellowsArray = new Array(candisView, jacquelynView, melissaView, aksharView, taylorView, stacyView, priyaView, kevinView, elizabethView, jayView, minhView, maryView, loriView, ronaView, nicholView, bethView, michaelView);
 
 var tab;
 var img;
@@ -355,7 +394,7 @@ var alumniFellowsArray = fellowsArray;
 var newAlumniFellowsArray = [];
 
 for (var i = 0; i < alumniFellowsArray.length; i++) {
-    if (alumniFellowsArray[i].fellow_status == "alumni") {
+    if (alumniFellowsArray[i].fellow_status == "alumni" && alumniFellowsArray[i].fellow_type == "sif") {
         newAlumniFellowsArray.push(alumniFellowsArray[i]);
         // console.log(newAlumniFellowsArray);
     } 
