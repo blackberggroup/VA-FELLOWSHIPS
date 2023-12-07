b319978a48a17400fb2396264357a67c1f492aee
Ronald Ponferrada <ronaldponferrada@Ronalds-MacBook-Air.local>
2023-12-07 23:27:06 +0900

    added youtube popup

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -53,10 +53,6 @@ $('.scroll').on('click',function(e) {
 	});
 });
 
-$(function() {
-	$('.lazy').Lazy();
-});
-
 $('.owl-carousel.current--fellows').owlCarousel({
     loop: true,
     margin: 10,
@@ -109,4 +105,16 @@ $('.owl-carousel.home').owlCarousel({
 });
 
 $(".owl-stage").addClass("display-flex flex-align-stretch");
+
+$('.vha-video__play-action').magnificPopup({
+    type: 'iframe', 
+    
+    iframe: {
+       markup: '<div class="mfp-iframe-scaler">'+
+                  '<div class="mfp-close"></div>'+
+                  '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                  '<div class="mfp-title">Some caption</div>'+
+                '</div>'
+    }
+});
 
