8987ea7eedea795dd70ff4ee61362e45b8382dfa
RyanCrux <ryan.loper@thecruxfirm.com>
2024-04-23 12:33:07 -0400

    fix: overlapping ticket that was not merged before

--- _site/assets/js/mustache-fellows-render.js
+++ _site/assets/js/mustache-fellows-render.js
@@ -53,7 +53,7 @@ let melissaView = {
     office_location: "Orlando VA Healthcare System",
     office_title2: "",
     office_location2: "",
-    project: "Perinatal Reproductive Education Planning and Resources (PREPARe)",
+    project: "Perinatal Reproductive Education Planning and Resources (PREPARe)", 
     bio: "Melissa Tran is a Marriage and Family Therapist and the Military Sexual Trauma Coordinator at Orlando VA Healthcare System. She leads the Perinatal Reproductive Education Planning and Resources (PREPARe) service, which has garnered national recognition through the achievement of the 2021 Dr. Robert L Jesse Award for Innovation. Ms. Tran is also an instructor for Nova Southeastern Kiran C Patel and University of Central Florida Colleges of Medicine and supports the diversity, equity, and inclusion team at OVAHCS.",
     bio2: "Perinatal Reproductive Education Planning and Resources (PREPARe) is a multidisciplinary approach to meet the needs of our perinatal Veterans. The innovation was developed due to a lack of comprehensive and easily accessible perinatal services available to Veterans. Services are scattered between VA, Community Care or not available at all. Veterans must pay out of pocket and request reimbursement for such services as lactation counseling covered under the Maternity Care benefit. PREPARe is a one stop shop wraparound approach where services typically offered in the community are now offered by trained VA specialists. These services include whole health, mental health, nutrition, physical therapy, women's health, chaplain care, and more."
 };
