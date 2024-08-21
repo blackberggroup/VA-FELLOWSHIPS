43407e869c89b72d6730e05304e4454bd7c2f505
ecstech-admin <james.strommer@ecstech.com>
2024-08-21 11:14:23 -0700

    bug: add missing json fields to joshi, lewis

--- assets/js/mustache-fellows-render.js
+++ assets/js/mustache-fellows-render.js
@@ -255,6 +255,8 @@ let priyaView = {
     job_description: "",
     office_title: "Chief Health Informatics Officer",
     office_location: "San Francisco VA Medical Center",
+    office_title2: "",
+    office_location2: "",
     project: "Ensuring Veteran Food Security",
     bio: "Dr. Priya Joshi is the Chief Health Informatics Officer at the San Francisco VA Medical Center. She identifies where exclusions in code become exclusions & inconvenience in care. We shouldn't have to jump through hoops to get the care and resources we need; the best care should be the path of least resistance. She started her fellowship in 2021 working on developing analytics to proactively identify missed diagnoses of kidney disease, hypertension, and diabetes to proactively provide upstream medication therapy that prevented kidney disease progression to dialysis, heart attacks, and strokes. She moved forward in 2022 building an enterprise evaluation of food security and building a grocery delivery program called Foodshare that helped get food to Veterans experiencing food insecurity and homelessness. She took the skills from the fellowship in building partnerships to get regular and artificial intelligence to the point of care, so Veterans and staff get information and support when and where they need it."
 };
@@ -273,6 +275,8 @@ let stacyView = {
     job_description: "",
     office_title: "Health System Specialist",
     office_location: "VHA New England Center for Innovation Excellence (NECIE)",
+    office_title2: "",
+    office_location2: "",
     project: "Veterans Health Administration Innovation Ecosystem Optimization",
     bio: "Stacey Lewis is a Health System and Improvement Specialist at the VA New England Center for Innovation Excellence (NECIE) where she focuses on the Center's strategic operations. As a certified Lean Six Sigma Black Belt and data-driven strategic thinker, her Veterans Health Administration Innovation Ecosystem (VHA IE) Optimization project is taking a deep dive into the new VA Pathfinder program's processes and outcomes. Her ultimate goal is to examine and optimize the flow of innovative solutions throughout VHA Office of Healthcare Innovation and Learning; beginning at intake and concluding with the successful integration of those solutions into the appropriate Care Models of the healthcare delivery system, she seeks to operationalize the process so that the value of innovation can be extracted and realized. Utilizing a systematic approach to understanding performance, key areas are efficiency, effectiveness, and strategic alignment."
 };
